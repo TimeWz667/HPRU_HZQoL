522b4aef40f9716f12b7b689ed8d393fe1c25480
Chu-Chang Ku <timewz667@gmail.com>
2025-02-19 17:20:52 +0000

    Add qaly loss outputs

--- run/output statistics.R
+++ run/output statistics.R
@@ -6,14 +6,27 @@ source(here::here("R", "misc.R"))
 pars <- read_csv(here::here("pars", "pars_shortfall_orig.csv"))
 
 
+tab <- read_csv(here::here("docs", "tabs", "QALY_loss_stats_orig.csv"))
 
-pars %>% 
-  crossing(Age = c(50, 60, 70, 80, 90)) %>% 
+tab %>% pull(Index) %>% unique()
+
+tab_wider <- tab %>% 
+  filter(Index %in% c("QL00", "QL15", "QLH00", "QLH15")) %>% 
   mutate(
-    rate = r0 * exp(Age * ba1),
-    dur = 1 / rate * 365
+    value = sprintf("%.3f (%.3f - %.3f)", M, L, U) 
+  )  %>% 
+  select(Age, name = Index, value) %>% 
+  pivot_wider()
+
+
+tab_wider %>% 
+  filter(Age < 99) %>% 
+  rename(
+    `From perfect health, d = 0%` = QLH00,
+    `From perfect health, d = 15%` = QLH15,
+    `From population norm, d = 0%` = QL00,
+    `From population norm, d = 15%` = QL15
   ) %>% 
-  group_by(Age) %>% 
-  summarise(
-    across(c(rate, dur), amlu) 
-  )
+  write_csv(here::here("docs", "tabs", "QALY_loss_summary.csv"))
+
+
